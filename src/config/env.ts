cf9f106aab7fbcf6d1560592ab6e69d8be6481fb
juan beresiarte <juanberesiarte@gmail.com>
2025-12-30 21:21:21 -0300

    feat(auth): add JWT token generation to login controller and update environment schema

--- src/config/env.ts
+++ src/config/env.ts
@@ -10,7 +10,8 @@ const envSchema = z.object({
     NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
     PORT: z.coerce.number().min(1).max(65535).default(3000),
     TURSO_DATABASE_URL: z.string().nonempty(),
-    TURSO_AUTH_TOKEN: z.string().nonempty()
+    TURSO_AUTH_TOKEN: z.string().nonempty(),
+    JWT_SECRET: z.string().nonempty()
 });
 
 const parsedEnv = envSchema.safeParse(process.env);
--- src/modules/auth/controllers/login.ts
+++ src/modules/auth/controllers/login.ts
@@ -1,6 +1,8 @@
+import { env } from "@/config/env";
 import { makeController } from "@/shared/utils/makeController";
 import { Login } from "@/modules/auth/actions/Login";
 import { z } from "zod";
+import jwt from "jsonwebtoken";
 
 const body = z.object({
     username: z.string("El nombre de usuario es necesario.").nonempty("El nombre de usuario no debe estar vacio.").max(100),
@@ -14,8 +16,15 @@ export const login = makeController<typeof body, typeof params>(async function (
 
     const { username, password } = body!;
 
-    const user = await Login({ username, password });
+    const { password: _, ...user } = await Login({ username, password });
 
-    res.json(user);
+    const token = jwt.sign({ ...user }, env.JWT_SECRET, { expiresIn: "2DAY" });
+
+    res.status(200).json({
+        status: "success",
+        messages: "Credenciales correctas.",
+        token,
+        role: user.role
+    });
 
 }, body, params);
